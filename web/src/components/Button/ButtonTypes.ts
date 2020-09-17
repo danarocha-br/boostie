2936457b1e71b44b9c2aeda0124a14be95fe1e04
Dana Rocha <dana@danarocha.com.br>
2020-09-17 14:34:46 +0300

    feat: make button component

---
+++ web/src/components/Button/ButtonTypes.ts
@@ -0,0 +1,15 @@
+export const ButtonVariants = Object.freeze({
+  BLOCK: 'block',
+  ICON: 'icon',
+});
+
+export const ButtonSizes = Object.freeze({
+  SMALL: 'sm',
+  MEDIUM: 'md',
+  LARGE: 'lg',
+});
+
+export const ButtonThemes = Object.freeze({
+  PRIMARY: 'primary',
+  SUCCESS: 'success',
+});
--- web/src/components/Button/index.tsx
+++ web/src/components/Button/index.tsx
@@ -1,9 +1,105 @@
 import React from 'react';
+import { PseudoBox, Icon } from '@chakra-ui/core';
+import { darken, transparentize } from 'polished';
 
-// import { Container } from './styles';
+import { ButtonVariants, ButtonThemes, ButtonSizes } from './ButtonTypes';
 
-const Button: React.FC = () => {
-  return <button>test</button>;
+import colors from '../../styles/colors';
+
+interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  label?: string;
+  icon?: string;
+  size?: 'lg' | 'md' | 'sm' | undefined;
+  theme?: string;
+  variants?: 'icon' | 'block';
+  loading?: boolean;
+  disabled?: boolean;
+}
+
+const Button: React.FC<IButtonProps> = ({
+  label,
+  icon,
+  size,
+  variants,
+  onClick,
+  theme,
+  disabled,
+}) => {
+  // button sizes
+  function renderSizes(size: string | undefined) {
+    switch (size) {
+      case ButtonSizes.LARGE:
+        return '60px';
+
+      case ButtonSizes.SMALL:
+        return '30px';
+
+      default:
+        return '45px';
+    }
+  }
+  // button theme
+
+  function renderTheme(theme: string | undefined) {
+    switch (theme) {
+      case ButtonThemes.SUCCESS:
+        return colors.green[900];
+
+      default:
+        return colors.gray[900];
+    }
+  }
+
+  // button variants
+
+  return (
+    <PseudoBox
+      as="button"
+      height={renderSizes(size)}
+      lineHeight="1.2"
+      outline="none"
+      transition="all 0.3s ease-in-out"
+      border="transparent"
+      px={size === 'lg' ? 5 : 4}
+      rounded={size === 'lg' ? '15px' : '10px'}
+      fontSize={size === 'lg' ? '18px' : '16px'}
+      fontWeight="semibold"
+      bg={renderTheme(theme)}
+      color="white"
+      _hover={{
+        bg: darken(0.05, renderTheme(theme)),
+        transform: 'scale(1.02)',
+      }}
+      _active={{
+        bg: transparentize(0.13, renderTheme(theme)),
+        transform: 'scale(0.9)',
+      }}
+      _focus={{
+        boxShadow:
+          theme === 'success'
+            ? '0 0 1px 2px rgba(0, 170, 167, .75), 0 1px 1px rgba(0, 0, 0, .15)'
+            : '0 0 1px 2px rgba(6, 36, 246, .75), 0 1px 1px rgba(0, 0, 0, .15)',
+      }}
+    >
+      {variants === 'icon' && (
+        <Icon
+          name={icon}
+          color="white"
+          size={size === 'lg' ? '22px' : '14px'}
+        ></Icon>
+      )}
+      {variants === 'block' && label}
+    </PseudoBox>
+  );
+};
+
+Button.defaultProps = {
+  variants: 'icon',
+  label: '',
+  disabled: false,
+  size: 'md',
+  theme: ButtonThemes.PRIMARY,
+  icon: '',
 };
 
 export default Button;
