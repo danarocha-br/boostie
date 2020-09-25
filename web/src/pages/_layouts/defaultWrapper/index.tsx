6ba89574aefb29fbf55564ce81bab63eb7bb1bb0
Dana Rocha <dana@danarocha.com.br>
2020-09-25 20:08:23 +0300

    :lipstick: style: make login page layout

--- web/src/pages/_layouts/defaultWrapper/index.tsx
+++ web/src/pages/_layouts/defaultWrapper/index.tsx
@@ -1,26 +1,59 @@
 import React from 'react';
 
-import { Flex, PseudoBox } from '@chakra-ui/core';
+import { Flex, PseudoBox, Heading } from '@chakra-ui/core';
 
 const defaultWrapper: React.FC = ({ children }) => {
   return (
     <Flex
       w="100%"
       h="100vh"
-      px={6}
       as="main"
-      flexDirection="column"
       alignItems="center"
-      justifyContent="center"
-      height="100vh"
-      bgImg=" radial-gradient( circle farthest-corner at 10% 20%,  rgba(234,249,249,0.8) 0.1%, rgba(239,249,251,0.7) 90.1% );"
+      justifyContent="space-between"
+      flexDirection={['column', 'column', 'column', 'row']}
     >
-      {children}
+      <Flex
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        px={6}
+        flex="1"
+        marginTop={['-30vh', '-50vh', '-50vh', '0']}
+      >
+        {children}
 
-      <PseudoBox position="absolute" bottom={['15px', '40px']}>
-        Developed and designed with ♥ by{' '}
-        <a href="http://www.linkedin.com/en/danarocha">Dana Rocha</a>
-      </PseudoBox>
+        <PseudoBox
+          justifyContent="center"
+          position="absolute"
+          bottom={['15px', '40px']}
+          textAlign="center"
+        >
+          Developed and designed with ♥ by{' '}
+          <a href="http://www.linkedin.com/en/danarocha">Dana Rocha</a>
+        </PseudoBox>
+      </Flex>
+
+      <Flex
+        h={['170px', '190px', '50vh', '100vh']}
+        w={['100%', '100%', '100%', '700px']}
+        alignItems={['flex-start', 'flex-start', 'flex-start', 'center']}
+        py={[8]}
+        justifyContent={['center']}
+        backgroundImage="url(https://res.cloudinary.com/danarocha/image/upload/v1601052327/github/bck-boostie_bfwwn6.jpg)"
+        backgroundSize="cover"
+        backgroundColor="#3a2a6b"
+        order={[-1, -1, -1, 0]}
+        style={{ backgroundBlendMode: 'hard-light' }}
+      >
+        <Heading
+          color="white"
+          fontWeight="regular"
+          fontSize="xl"
+          display={['none', 'block', 'block']}
+        >
+          Finance, open for everyone.
+        </Heading>
+      </Flex>
     </Flex>
   );
 };
